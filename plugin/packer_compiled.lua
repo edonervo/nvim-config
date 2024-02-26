4eeec7626893daaecab11b21ee89bbfcb5b83803
Edoardo Nervo <edoardonervo96@gmail.com>
2024-02-26 11:00:15 +0100

    add WhichKey

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -263,6 +263,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/edo/.local/share/nvim/site/pack/packer/start/vscode.nvim",
     url = "https://github.com/Mofiqul/vscode.nvim"
+  },
+  ["which-key.nvim"] = {
+    loaded = true,
+    path = "/home/edo/.local/share/nvim/site/pack/packer/start/which-key.nvim",
+    url = "https://github.com/folke/which-key.nvim"
   }
 }
 
